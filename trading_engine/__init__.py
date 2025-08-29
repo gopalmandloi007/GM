c454c8b941c6d58fb5c4b654ff5a74b77d9518ee
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-08-29 20:45:46 +0530

    Update __init__.py

--- trading_engine/__init__.py
+++ trading_engine/__init__.py
@@ -1,3 +1,5 @@
+# gm/trading_engine/__init__.py
+
 from .api_client import APIClient
 from .session import SessionManager, SessionError
-from .orders import place_order
+from .orders import OrderManager   # ✅ fix
